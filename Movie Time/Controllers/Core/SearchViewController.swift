5a38b6f140f287ff4d76a21c7c081827a4facfef
YuriiKruk <pmyurk@gmail.com>
2022-01-15 19:10:43 +0200

    add core view controllers

---
+++ Movie Time/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 08.12.2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie Time/Controllers/Core/TVShowViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TVViewController.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 08.12.2021.
+//
+
+import UIKit
+
+class TVShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
