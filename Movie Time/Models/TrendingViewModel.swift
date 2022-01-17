2a210a5a6230e950d99a304dbab396de2fa889d7
YuriiKruk <pmyurk@gmail.com>
2022-01-17 18:47:39 +0200

    added TrendingViewModel

---
+++ Movie Time/Models/TrendingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TrendingViewModel.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 17.01.2022.
+//
+
+import Foundation
+
+struct TrendingViewModel {
+    let section: String
+    let movie: [Movie]
+}
