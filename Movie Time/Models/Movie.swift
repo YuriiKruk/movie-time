f6d06054d2de5bdae6a863a1094dd34f2914d08c
YuriiKruk <pmyurk@gmail.com>
2022-01-17 18:47:10 +0200

    added movie model

---
+++ Movie Time/Models/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 16.01.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Movie {
+    let poster: UIImage
+    let name: String
+    let description: String
+    let releaseDate: String
+    let duration: Int
+    let rating: Double
+}
