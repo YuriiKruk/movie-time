553e1c0900314be6107a775d9f7cc04dc1f90abb
YuriiKruk <pmyurk@gmail.com>
2022-01-20 01:03:48 +0200

    separated Constants into its own file

---
+++ Movie Time/Managers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 20.01.2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseAPIURL = "https://api.themoviedb.org/3"
+    static let apiKey = "?api_key=c5115d2fd4b5c7908af39275bad3df28"
+    static let baseImageURL = "https://image.tmdb.org/t/p/w500"
+    static let imagePlaceholder = "clock.arrow.2.circlepath"
+}
