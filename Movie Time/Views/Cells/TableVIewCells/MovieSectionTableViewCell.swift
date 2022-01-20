5664978541556d8064c29898a3609507b167e3a6
YuriiKruk <pmyurk@gmail.com>
2022-01-20 02:37:42 +0200

    created MovieSectionTableViewCell

---
+++ Movie Time/Views/Cells/TableVIewCells/MovieSectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieSectionTableViewCell.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 20.01.2022.
+//
+
+import UIKit
+
+class MovieSectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
