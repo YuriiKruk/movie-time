f80e815f2668249ef62e633224dbb0c291478fd9
YuriiKruk <pmyurk@gmail.com>
2022-01-20 15:39:00 +0200

    updated constants

--- Movie Time/Managers/Constants.swift
+++ Movie Time/Managers/Constants.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
+import UIKit
 
-struct Constants {
+enum Constants {
     static let baseAPIURL = "https://api.themoviedb.org/3"
     static let apiKey = "?api_key=c5115d2fd4b5c7908af39275bad3df28"
     static let baseImageURL = "https://image.tmdb.org/t/p/w500"
     static let imagePlaceholder = "clock.arrow.2.circlepath"
+}
+
+enum Theme {
+    static let padding: CGFloat = 10
 }
