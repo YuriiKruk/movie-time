b62343d795f7b691ede2b342ab5be627151976a0
YuriiKruk <pmyurk@gmail.com>
2022-01-25 04:07:15 +0200

    minor updates NewMoviePosterTableViewCell

--- Movie Time/Views/Cells/TableVIewCells/NewMoviePosterTableViewCell.swift
+++ Movie Time/Views/Cells/TableVIewCells/NewMoviePosterTableViewCell.swift
@@ -9,32 +9,27 @@ import UIKit
 
 // MARK: - NewMoviePosterTableViewCellDelegate
 protocol NewMoviePosterTableViewCellDelegate: AnyObject {
-    func didTapPoster(cellView: NewMoviePosterTableViewCell, model: Media)
+    func didTapPoster(cellView: NewMoviePosterTableViewCell, model: Movie)
 }
 
-class NewMoviePosterTableViewCell: UITableViewCell {
-    
-    // MARK: - Static Identifiers
-    static let identifier = "NewMoviePosterTableViewCell"
-    static func nib() -> UINib {
-        return UINib(nibName: "NewMoviePosterTableViewCell", bundle: nil)
-    }
+class NewMoviePosterTableViewCell: UITableViewCell, NibSetapable {
     
     // MARK: Delegate
     weak var delegate: NewMoviePosterTableViewCellDelegate?
     
-    // MARK: - Properties
-    @IBOutlet var collectionView: UICollectionView!
+    // MARK: - IBOutlets
+    @IBOutlet private var collectionView: UICollectionView!
     
+    private let sectionInset: UIEdgeInsets = UIEdgeInsets(top: 0, left: Theme.padding, bottom: 0, right: Theme.padding)
     /// Size for  collection view cell
-//    lazy private var cellSize = CGSize(width: (contentView.frame.width / 2) - 20, height: contentView.frame.height)
-    lazy private var cellSize = CGSize(width: contentView.frame.height * 0.6, height: contentView.frame.height)
+    /// - The item height must be less than the height of the UICollectionView minus the section insets top and bottom values, minus the content insets top and bottom values
+    lazy private var cellSize = CGSize(width: contentView.frame.height * 0.6, height: contentView.frame.height - 1)
     
     // MARK: Model
-    private var model = [Media]()
+    private var model = [Movie]()
     
     // MARK: - Configure Cell
-    public func configure(model: [Media]) {
+    public func configure(model: [Movie]) {
         self.model = model
     }
     
@@ -56,7 +51,7 @@ class NewMoviePosterTableViewCell: UITableViewCell {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.collectionView?.isPagingEnabled = true
-        layout.sectionInset = UIEdgeInsets(top: 0, left: Theme.padding, bottom: 0, right: Theme.padding)
+        layout.sectionInset = sectionInset
         collectionView.collectionViewLayout = layout
     }
 }
@@ -64,7 +59,7 @@ class NewMoviePosterTableViewCell: UITableViewCell {
 // MARK: - Setup Collection View Delegate & Data Sourse Delegate
 extension NewMoviePosterTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return model.count
+        model.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -78,7 +73,7 @@ extension NewMoviePosterTableViewCell: UICollectionViewDelegate, UICollectionVie
         guard let cell = collectionView.cellForItem(at: indexPath) as? MoviePosterCollectionViewCell else {
             return
         }
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseIn) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseInOut) {
             cell.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         } completion: { _ in
             UIView.animate(
@@ -100,7 +95,7 @@ extension NewMoviePosterTableViewCell: UICollectionViewDelegate, UICollectionVie
 // MARK: - Setup Collection View Flow Layout Delegate
 extension NewMoviePosterTableViewCell: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return cellSize
+        cellSize
     }
 }
 
