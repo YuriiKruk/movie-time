7a35637e1cfb78a5ab76cdfe9f4ce4987a0c7c7d
YuriiKruk <pmyurk@gmail.com>
2022-01-25 04:05:52 +0200

    created MediaViewModel.swift

---
+++ Movie Time/ViewModels/MediaViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MediaViewModel.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 20.01.2022.
+//
+
+import Foundation
+
+struct MediaViewModel {
+    let posterURL: String?
+    
+    let title: String
+    let releaseDate: String?
+    let overview: String
+    let rating: String
+    let popularity: Double?
+    let adult: Bool?
+    
+    let genres: [String]
+}
