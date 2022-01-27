57bdd1829ad23473fe42e4663e55861822499601
YuriiKruk <pmyurk@gmail.com>
2022-01-27 04:05:01 +0200

    created MediaPosterCollectionReusableView

---
+++ Movie Time/Views/Cells/CollectionViews/MediaPosterCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPosterCollectionReusableView.swift
+//  Movie Time
+//
+//  Created by Yury Kruk on 27.01.2022.
+//
+
+import UIKit
+
+class MediaPosterCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
