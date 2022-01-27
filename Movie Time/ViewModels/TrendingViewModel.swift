b6824f4b048477b6733be3eb2b4e98847639d53a
YuriiKruk <pmyurk@gmail.com>
2022-01-27 03:50:54 +0200

    created TrendingSectionViewModel enum

--- Movie Time/ViewModels/TrendingViewModel.swift
+++ Movie Time/ViewModels/TrendingViewModel.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 struct TrendingViewModel {
-    let section: String
+    let section: TrendingSectionViewModel
     let movie: [Movie]
+}
+
+enum TrendingSectionViewModel: String {
+    case nowTrending = "Now Trending"
+    case recommended = "Recommended"
+    case popular = "Popular"
+    case upcoming = "Upcoming"
 }
